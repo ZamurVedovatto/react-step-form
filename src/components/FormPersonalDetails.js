24497637b4e2d4afcf58193f4a5b1f4e9ae82436
Zamur Vedovatto <vedovatto@gmail.com>
2020-05-21 23:07:36 -0300

    create components

---
+++ src/components/FormPersonalDetails.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class FormPersonalDetails extends Component {
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default FormPersonalDetails;
---
+++ src/components/UserForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class UserForm extends Component {
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default UserForm;
